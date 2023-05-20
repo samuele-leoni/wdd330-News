162c1f42f4a0dc540cf4f337c46184b92bf7b6b6
Samuele Leoni <leo22009@byui.edu>
2023-05-21 00:28:50 +0200

    Added main js and implemented header and footer injection

---
+++ src/js/utils.mjs
@@ -0,0 +1,22 @@
+async function loadTemplate(path) {
+    const html = await fetch(path);
+    const template = await html.text();
+    return template;
+}
+
+export function renderWithTemplate(template, parentElement, data, callback) {
+    parentElement.insertAdjacentHTML("afterbegin", template);
+    if (callback) {
+        callback(data);
+    }
+}
+
+export async function loadHeaderFooter() {
+    const path = "/public/partials/"
+    let element = document.querySelector("header");
+    let templateStr = await loadTemplate(`${path}header.html`);
+    renderWithTemplate(templateStr, element)
+    element = document.querySelector("footer");
+    templateStr = await loadTemplate(`${path}footer.html`);
+    renderWithTemplate(templateStr, element)
+}
