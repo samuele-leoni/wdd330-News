7b66767027dfcffe4207ac1a37cb6f0cb53905c4
Samuele Leoni <leo22009@byui.edu>
2023-05-30 12:13:03 +0200

    Finished implementing unit change

--- src/js/Weather.mjs
+++ src/js/Weather.mjs
@@ -1,8 +1,8 @@
-function weatherTemplate(weatherData) {
+function weatherTemplate(weatherData, metric) {
     const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`;
     const desc = weatherData.weather[0].description;
 
-    return `<img id="weather-icon" src="${iconsrc}" alt="${desc}"><span id="temperature">${weatherData.main.temp.toFixed(0)}°C</span>`;
+    return `<img id="weather-icon" src="${iconsrc}" alt="${desc}"><span id="temperature">${weatherData.main.temp.toFixed(0)}${metric?"°C":"°F"}</span>`;
 }
 
 export default class Weather {
@@ -11,23 +11,27 @@ export default class Weather {
         this.dataSource = dataSource;
     }
 
-    async init(selector, city="London", metric = false) {
+    async init(selector, city = "London", metric = false) {
         this.weatherData = await this.dataSource.getCurrentWeather(city, metric);
-        this.renderWeather(selector);
+        this.renderWeather(selector, metric);
     }
 
-    async initByCoords(selector, citySelector, lat=0, long=0, metric = false) {
+    async initByCoords(selector, citySelector, lat = 0, long = 0, metric = false) {
         this.weatherData = await this.dataSource.getCurrentWeatherByCoords(lat, long, metric);
         citySelector.value = this.weatherData.name;
-        this.renderWeather(selector);
+        this.renderWeather(selector, metric);
     }
 
-    renderWeather(selector) {
+    getWeatherUnit() {
+        return this.unit;
+    }
+
+    renderWeather(selector, metric) {
         const element = document.querySelector(selector);
         element.innerHTML = "";
         element.insertAdjacentHTML(
             "afterBegin",
-            weatherTemplate(this.weatherData)
+            weatherTemplate(this.weatherData, metric)
         );
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -6,17 +6,29 @@ import NewsServices from "./NewsServices.mjs";
 
 loadHeaderFooter();
 
+/*
+    WEATHER
+*/
+
 const weatherDataSource = new WeatherServices();
 
 let weatherLocation = document.querySelector("#weather-location-input");
 
+let metric = JSON.parse(localStorage.getItem("metric"));
+
+if(metric === null) {
+    metric = true;
+}
+
 const weatherLocationButtonCity = document.querySelector("#weather-location-search-button");
 const weatherLocationButton = document.querySelector("#weather-location-button");
+const weatherMetricButton = document.querySelector("#unit-celsius");
+const weatherImperialButton = document.querySelector("#unit-fahrenheit");
 
-const weather = new Weather(weatherDataSource, true);
+const weather = new Weather(weatherDataSource);
 
 navigator.geolocation.getCurrentPosition(async (position) => {
-    await weather.initByCoords("#weather-container", weatherLocation, position.coords.latitude, position.coords.longitude, true, true);
+    await weather.initByCoords("#weather-container", weatherLocation, position.coords.latitude, position.coords.longitude, metric, true);
 });
 
 if(weatherLocation.value === "") {
@@ -25,18 +37,35 @@ if(weatherLocation.value === "") {
 
 weatherLocationButton.addEventListener("click", () => {
     navigator.geolocation.getCurrentPosition(async (position) => {
-        await weather.initByCoords("#weather-container", weatherLocation, position.coords.latitude, position.coords.longitude, true, true);
+        await weather.initByCoords("#weather-container", weatherLocation, position.coords.latitude, position.coords.longitude, metric, true);
     });
 });
 
 weatherLocationButtonCity.addEventListener("click", () => {
-    weather.init("#weather-container", weatherLocation.value, true);
+    weather.init("#weather-container", weatherLocation.value, metric);
 });
 
-weather.init("#weather-container", weatherLocation.value, true);
+weather.init("#weather-container", weatherLocation.value, metric);
+
+weatherMetricButton.addEventListener("click", async () => {
+    metric = true;
+    localStorage.setItem("metric", JSON.stringify(metric));
+    await weather.init("#weather-container", weatherLocation.value, metric);
+});
+
+weatherImperialButton.addEventListener("click", async () => {
+    metric = false;
+    localStorage.setItem("metric", JSON.stringify(metric));
+    await weather.init("#weather-container", weatherLocation.value, metric);
+});
+
+/*
+    NEWS
+*/
 
 const newsDataSource = new NewsServices();
 
 const news = new News(newsDataSource);
 
 news.init("#news-container");
+
